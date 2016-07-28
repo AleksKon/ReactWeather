2ad145f5fc46fb1767400390dd54f7b3c1ba8070
Aleksandra <koneska.aleksandra@gmail.com>
2016-07-28 11:13:11 -0400

    Rename example page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,23 +1,10 @@
 var React = require('react');
 
-
-// var Examples = React.createClass({
-// 	render: function(){
-// 		return(
-// 			<div>
-// 				<h3>Examples Component</h3>
-				
-// 			</div>
-			
-// 		);
-// 	}
-// });
-
 var Examples = (props) => {
 	return(
 			<div>
-				<h2>Examples Component</h2>
-				<p>Welcome to the examples page. This is an example component.</p>
+				<h2>Examples</h2>
+				<p>Welcome to the examples page.</p>
 			</div>
 			
 		);
