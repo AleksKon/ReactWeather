cdffd88dfc3cef2eea807e622655ced9a39fd130
Aleksandra <koneska.aleksandra@gmail.com>
2016-07-29 14:23:29 -0400

    Add costum styles

--- app/app.jsx
+++ app/app.jsx
@@ -10,8 +10,11 @@ var WeatherForm = require('WeatherForm');
 var WeatherMsg = require('WeatherMsg');
 
 // Load foundation
-require('style!css!foundation-sites/dist/foundation.min.css')
+require('style!css!foundation-sites/dist/foundation.min.css');
 $(document).foundation();
+
+//app css
+require('style!css!applicationStyles');
 
 ReactDOM.render(
 	<Router history = {hashHistory}>
--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,7 +4,7 @@ var {Link} = require('react-router');
 var About = (props) => {
 	return(
 			<div className="row">
-				<h1 className='text-center'>About</h1>
+				<h1 className='text-center page-title'>About</h1>
 				<p>This is a weather application built on React. I have bulit this for the Complete React Web App Developer Course.
 				</p>
 				<p>Here are some of the tools I used:</p>
